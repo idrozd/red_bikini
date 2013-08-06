93dc7e9486b5e89f93a9716ccf2fad60f102042f
Andrey Titov <terracote@gmail.com>
2013-08-06 09:49:15 +0400

    Non-setter covered

--- some_spec.rb
+++ some_spec.rb
@@ -1,7 +1,12 @@
 require_relative 'code'
 
 describe RedBikini do
-  Person = Struct.new(:id, :name, :friends)
+  Person = Struct.new(:id, :name, :friends, :mood) do
+    def im_a_girl! traits
+      self.name = traits.fetch :name
+      self.mood = traits.fetch :mood
+    end
+  end
 
   before{RedBikini.add_to_wardrobe! Person}
 
@@ -11,16 +16,28 @@ describe RedBikini do
       individual.id = 0b101_010
       individual.name = 'Frank'
       individual.friends = ['Susan', 'Rajesh']
+      individual.mood    = :good
     end
 
     frank = Person.new.in_bikini do
       id_is 42
       name_is 'Frank'
       friends_are %w[Susan Rajesh]
+      set_mood :good
     end
 
     expect(frank).to eq individual
+
+    individual.tap do |individual|
+      individual.im_a_girl! name: 'Josephine', mood: :bad
+    end
+
+    frank.in_public{ im_a_girl! name: 'Josephine', mood: :bad}
+
+    expect(frank).to eq individual
   end
+
+
 
 
 end
