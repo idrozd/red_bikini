3f91d43e4f55bf067f5bf47b97c7f94ee94e2c07
Andrey Titov <terracote@gmail.com>
2013-08-08 01:31:26 +0400

    Specs for access, typing and closure stuff Code split to more focused parts

--- code.rb
+++ code.rb
@@ -1,19 +1,44 @@
+require 'active_support/core_ext/module/delegation'
 module RedBikini
 
-  def self.add_to_wardrobe! el_class=Object
-    el_class.module_exec do
-      def in_public &speech
-        tap{Bikini.new(self).instance_exec &speech}
+  def self.add_to_wardrobe! klass=Object
+    klass.module_exec do
+      def in_public &block
+        ::RedBikini::Make.in_public(self,&block)
       end
       alias in_bikini in_public
+      def expose &block
+        ::RedBikini::Make.expose(self,&block)
+      end
+      alias tell expose
+      alias confide expose
     end
 
+    klass.instance_exec do
+      def such_that *args, &description
+        ::RedBikini::Make.in_public(new(*args),&description)
+      end
+    end
+
+  end
+
+  module Make
+    module_function
+    def in_public receiver, &block
+      expose(receiver, &block)
+      receiver
+    end
+    def expose receiver, &block
+      Bikini.new(receiver).instance_exec &block
+    end
   end
 
   class Bikini
     def initialize object
       @object = object
     end
+    delegate :is_a?, :kind_of?, :===, :class, to: :@object
+
     def object_method method
       setter_imitated_by(method) or method
     end
@@ -23,6 +48,7 @@ module RedBikini
     def method_missing method, *args
       @object.public_send(object_method(method), *args)
     end
+
 
     def setter_imitated_by method
       [/set_(?<attr>\w+)/,/(?<attr>\w+)(_are|_is)/].find do |setter_matcher|
--- some_spec.rb
+++ some_spec.rb
@@ -24,7 +24,7 @@ describe RedBikini do
       individual.mood    = :good
     end
 
-    frank = Person.new.in_bikini do
+    frank = Person.such_that do
       id_is 42
       name_is 'Frank'
       friends_are %w[Susan Rajesh]
@@ -37,23 +37,51 @@ describe RedBikini do
       individual.im_a_girl! name: 'Josephine', mood: :bad
     end
 
-    frank.in_public{ im_a_girl! name: 'Josephine', mood: :bad}
+    frank.in_public{im_a_girl! name: 'Josephine', mood: :bad}
 
     expect(frank).to eq individual
   end
 
 
-  specify do
-    josephine = Person.new(1,'Josephine')
-    expect do
-      josephine.in_bikini{expose_rich_inner_world}
-    end.to raise_error(NoMethodError, /private/)
-    expect do
-      josephine.in_public{being_told_stupid_jokes}
-    end.to raise_error NoMethodError, /protected/
+  specify 'confide,tell and expose return block result' do
+    person = Person.such_that{name_is 'Daniel'; friends_are %w[Mary Steven]; mood_is :great}
+
+    expect(person.tell{"#{name} and #{friends.join ' and '} are friends"})
+    .to eq 'Daniel and Mary and Steven are friends'
+
+    expect(person.confide{"I feel #{mood}!"}).to eq 'I feel great!'
   end
 
 
+  describe 'access' do
+    let(:josephine){ Person.new(1,'Josephine')}
+    specify do
+      expect{josephine.tell{credit_card_number}}
+      .to raise_error(NoMethodError, /private/)
+    end
+    specify do
+      expect{josephine.expose{girly_secrets}}
+      .to raise_error NoMethodError, /protected/
+    end
+  end
 
 
+  describe 'hacked typing' do
+    let(:a_person){Person.new(1,'Joe')}
+    specify{expect(a_person.tell{self.is_a? Person}).to be_true}
+    specify{expect(a_person.tell{is_a? Person}).to be_true}
+    specify{expect(a_person.tell{self.class}).to eq Person}
+    specify{expect(a_person.tell{kind_of? Person}).to be_true}
+  end
+
+  describe 'closure stuff' do
+    let(:joe){Person.new(1,'Joe')}
+    HOST_CONST = 'Joe is best'
+    example{name = 'Local Joe'; expect(joe.tell{name}).to eq 'Local Joe'}
+    context 'there is caller method with same name' do
+      let(:name){'Host Joe'}
+      specify{expect(joe.tell{name}).to eq 'Joe'}
+    end
+    specify{expect(joe.tell{HOST_CONST}).to eq 'Joe is best'}
+  end
 end
