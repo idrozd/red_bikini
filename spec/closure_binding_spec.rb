9fc530a157e8592560ba80e958a823bbf5536eda
Andrey Titov <terracote@gmail.com>
2013-08-12 08:18:38 +0400

    Closure binding const better example

--- spec/closure_binding_spec.rb
+++ spec/closure_binding_spec.rb
@@ -5,13 +5,19 @@ describe RedBikini do
 
   describe 'closure stuff' do
     let(:joe){Person.new(1,'Joe')}
-    HOST_CONST = 'Joe is best'
     example{name = 'Local Joe'; expect(joe.tell{name}).to eq 'Local Joe'}
     context 'there is caller method with same name' do
       let(:name){'Host Joe'}
       specify{expect(joe.tell{name}).to eq 'Joe'}
     end
-    specify{expect(joe.tell{HOST_CONST}).to eq 'Joe is best'}
+    context do
+      HOST_CONST = 'Joe is best'
+      specify{expect(joe.tell{HOST_CONST}).to eq 'Joe is best'}
+    end
+    context 'Host const shadows own', :gotcha do
+      before{stub_const 'OWN_CONST', 'Shadowing person const'}
+      specify{(expect joe.tell{OWN_CONST}).to eq 'Shadowing person const'}
+    end
     specify{expect(joe.tell{OWN_CONST}).to eq 'Person const'}
   end
 
